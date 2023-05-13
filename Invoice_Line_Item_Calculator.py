d20ca6fe5426a128c12584317c1d9551af797555
username <useremail>
2023-05-12 18:39:00 -0700

    All Code / Errors

--- Invoice_Line_Item_Calculator.py
+++ Invoice_Line_Item_Calculator.py
@@ -1,1 +1,39 @@
 # Alex Crider - CIS261 - Invoice Line-Item Calculator 
+
+def get_price():    
+    while True:
+        try:
+            price = float(input("Enter price: "))
+            return price
+        except ValueError:
+            print("Invalid decimal number. Please try again.")
+
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid integer. Please try again.")
+
+def main():
+    print("The invoice Line Item Calculator \n")
+
+    answer = "y"
+    while answer.lower() == "y":
+        price = get_price()
+        quantity = get_quantity
+
+        total = price * quantity
+
+        print()
+        print("PRICE:    ", f"{price:  .2f}")
+        print("QUANTITY:    ", f"{total:  .2f}")
+        print("TOTAL:    ", f"{total:  .2f}")
+        answer = input("Enter another line item? y/n: ")
+
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
